9fa6f778fec19d20bcd69986e32d2c214bdb8211
Ox Cart <ox.to.a.cart@gmail.com>
2015-02-18 08:50:27 -0600

    [gost] Added github.com/cratonica/2goarray and its dependencies

---
+++ src/github.com/getlantern/autoupdate/_test_app/config.go
@@ -0,0 +1,17 @@
+package main
+
+var configMap = map[string]*config{
+	"_test_app": &config{
+		appID:         `ap_71pTnk9r62keCb_MpgnIEP5N_v`,
+		updateChannel: `stable`,
+		publicKey: []byte(`-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA37sL7tGOroGj3zb/jL1A
+nhy0xYTzTbC1sw+pXuRlpsyhJl0WdaLteLDFW2HZnvTQaku/SZWWlB/Ax6yZ8eNa
+Ae88/Adn2Xlp8tEdVXVj//K4qjgWnDohCaWsziKpUY5dzRQ02icYRgPKr7gacp/T
+NOFeZ/Yxqf/E9DG896iRNJwvUwX6aHpJIYbs9vHHp0TB8eK+eE+3TwnadXr1Fiw9
+rWGT/Qla5Zy1xW9frl0jaHr81xFO2S9/x5aAGix2Az7AcB92geSiKfEAr/QGnq9k
+tiiBmR4CELfxLHPntNAZUM9HZfhf1vE8n2j5z+DBUnH0qlh6mPvcaGOpkGRNEYRL
+AQIDAQAB
+-----END PUBLIC KEY-----`),
+	},
+}
