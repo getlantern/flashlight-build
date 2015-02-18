b7fde3867296723a0c1ffb6115042ed0b21dc850
Ox Cart <ox.to.a.cart@gmail.com>
2015-02-18 09:02:01 -0600

    Switched systray to use 2goarray

--- src/github.com/getlantern/systray/embeddll.bash
+++ src/github.com/getlantern/systray/embeddll.bash
@@ -6,5 +6,11 @@
 #
 ###############################################################################
 
-go get github.com/jteeuwen/go-bindata/go-bindata
-go-bindata -nomemcopy -nocompress -pkg systray -prefix dll -o systraydll_windows.go dll
+function die() {
+  echo $*
+  exit 1
+}
+which 2goarray > /dev/null || go get github.com/cratonica/2goarray/...
+which 2goarray > /dev/null || die "Please install 2goarray manually, then try again"
+
+2goarray systraydll systray < systray.dll > systraydll_windows.go
